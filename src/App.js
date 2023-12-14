6551c724b9f0d99fab86a37b094dca433a294e93
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-13 19:46:01 -0600

    se agrego un esqueleto de prueba

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <h1>Tablero</h1>
+      </div>
+      <div>
+        <div>
+          <h1>Tareas pendientes</h1>
+        </div>
+        <div>
+          <h1>Tareas completadas</h1>
+        </div>
+      </div>
     </div>
   );
 }
