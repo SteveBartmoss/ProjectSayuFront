824f3b91a2f46f8e6325fc3b6cc973d9702e07e1
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-13 19:54:35 -0600

    se agregaron los componentes de los contenedores

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { DivHead, DivLista, DivTablero } from './componentes/Cajas';
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <DivHead>
         <h1>Tablero</h1>
-      </div>
-      <div>
-        <div>
+      </DivHead>
+      <DivTablero>
+        <DivLista>
           <h1>Tareas pendientes</h1>
-        </div>
-        <div>
+        </DivLista>
+        <DivLista>
           <h1>Tareas completadas</h1>
-        </div>
-      </div>
+        </DivLista>
+      </DivTablero>
     </div>
   );
 }
