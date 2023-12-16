cfb51bd6102c44ae07b2a08c27aa7ea6f4c6cd07
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-15 20:11:38 -0600

    se dejo solo el router en la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import { RouterTablero } from './Routes/Router';
 import { DivHead, DivLista, DivTablero } from './componentes/Cajas';
 import { Footer } from './componentes/Footer';
+import { NavBar } from './componentes/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <DivHead>
-        <h1>Tablero</h1>
-      </DivHead>
-      <DivTablero>
-        <DivLista>
-          <h1>Tareas pendientes</h1>
-        </DivLista>
-        <DivLista>
-          <h1>Tareas completadas</h1>
-        </DivLista>
-      </DivTablero>
-      <Footer />
+      <RouterTablero />
     </div>
   );
 }
