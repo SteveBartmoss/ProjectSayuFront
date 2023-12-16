fc2eb8c3ec8f6f3fc4b2a1836c12ca9f48804a4e
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-16 15:33:54 -0600

    se implemento el contenedor de la barra de navegacion

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { DivNav } from "./Cajas";
 
-export function NavBar(){
-    return(
-        <div>
-            
+export function NavBar() {
+    return (
+        <DivNav>
             <NavLink to="home">Tablero</NavLink>
 
             <NavLink to="newtask" >Nueva Tarea</NavLink>
-
-        </div>
+        </DivNav>
     );
 }
