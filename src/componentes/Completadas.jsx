4cce49ab572c2baff58d62c774299e0270b4fedf
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 15:31:25 -0600

    se agrego un componente para listar las tareas pendientes

---
+++ src/componentes/Completadas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { DivLista } from "./Cajas";
+
+export function Completatas(){
+    return(
+        <>
+            <h1>Tareas Completadas</h1>
+            <DivLista>
+                
+            </DivLista>
+        </>
+    );
+}
