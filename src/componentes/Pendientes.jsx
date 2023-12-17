99cb2eca36fffd5485e309e3207689a52c6be653
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 15:59:19 -0600

    se agrego la lista de tareas y se uso un map para listar las tareas

--- src/componentes/Pendientes.jsx
+++ src/componentes/Pendientes.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { DivLista } from "./Cajas";
+import { CardTarea } from "./CardTarea";
 
 export function Pendientes(){
+
+    const [tareas,setTareas]=useState(
+        [
+            {Titulo:"Completar aplicacion", Descripcion:"Se debe completar la aplicacion del tablero"},
+            {Titulo:"Subir video", Descripcion:"Se debe subir el nuevo video a stevechambitas"},
+            {Titulo:"Jugar Elden Ring", Descripcion:"Se debe completar elden ring"},
+            {Titulo:"Completar pase", Descripcion:"Se debe completar el pase de batallas de fornite"},
+            {Titulo:"completar Miniaturas", Descripcion:"Se deben completar la miniaturas"},
+            {Titulo:"Ver proyecyo X", Descripcion:"Se debe terminar de ver proyecto X"},
+            {Titulo:"Terminar el libro", Descripcion:"Se debe completar el libro"},
+            {Titulo:"Formar la empresa", Descripcion:"Se debe formar la empresa devfarm"}
+        ]
+    );
+
     return(
         <>
             <h1>Tareas Pendientes</h1>
             <DivLista>
-                
+                {
+                    tareas.map(
+                        tarea=><CardTarea tarea={tarea} />
+                    )
+                }
             </DivLista>
         </>
     );
