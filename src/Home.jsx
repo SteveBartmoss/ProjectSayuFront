8a5fbe0f133cda8c8fdd6d941265da7f19beb872
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 15:56:42 -0600

    se agregaron las listas de tareas pendientes y completadas

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { DivTablero } from "./componentes/Cajas";
+import { Pendientes } from "./componentes/Pendientes";
+import { Completadas } from "./componentes/Completadas";
 
 export function Home() {
     return (
         <DivTablero>
             <h1>home XD</h1>
+            <Pendientes />
+            <Completadas />
         </DivTablero>
     );
 }
