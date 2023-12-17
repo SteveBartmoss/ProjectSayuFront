e7634d38fa7194a11bb2506bda8bb1443ec24310
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 15:31:00 -0600

    se agregaron los imputs para crear una tarea, se agrego el componente de div tablero

--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { DivTablero } from "./componentes/Cajas";
 
-export function CreateTask(){
-    return(
-        <div>
+export function CreateTask() {
+    return (
+        <DivTablero>
             <h1>Crear tarea</h1>
-        </div>
+            <label>Titulo</label>
+            <input type="text"></input>
+            <label>Descripcion</label>
+            <input type="text"></input>
+        </DivTablero>
     );
 }
