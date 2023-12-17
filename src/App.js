c5f9f9fe2f549cee6511268b5b24cc54f39d0eec
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 15:09:23 -0600

    se cambio el contenedor padre por el propio paper

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { RouterTablero } from './Routes/Router';
-import { DivHead, DivLista, DivTablero } from './componentes/Cajas';
+import { DivHead, DivLista, DivTablero, Paper } from './componentes/Cajas';
 import { Footer } from './componentes/Footer';
 import { NavBar } from './componentes/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <Paper>
       <RouterTablero />
-    </div>
+    </Paper>
   );
 }
 
