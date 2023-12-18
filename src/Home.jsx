b4388b0bc341fe08810a8fb6825c12c9446172ed
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 19:35:52 -0600

    se agrego el componente grid lista

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { DivTablero } from "./componentes/Cajas";
+import { DivTablero, GridListas } from "./componentes/Cajas";
 import { Pendientes } from "./componentes/Pendientes";
 import { Completadas } from "./componentes/Completadas";
 
 export function Home() {
     return (
         <DivTablero>
-            <h1>home XD</h1>
-            <Pendientes />
-            <Completadas />
+            <GridListas>
+                <Pendientes />
+                <Completadas />
+            </GridListas>
         </DivTablero>
     );
 }
