de3a361949410b318983e012d54c3027f0d2b2e3
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-31 21:02:33 -0600

    se agrego el componente del footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DivTablero, GridListas } from "./componentes/Cajas";
 import { Pendientes } from "./componentes/Pendientes";
 import { Completadas } from "./componentes/Completadas";
+import { Footer } from "./componentes/Footer";
 
 export function Home() {
     return (
@@ -10,6 +11,7 @@ export function Home() {
                 <Pendientes />
                 <Completadas />
             </GridListas>
+            <Footer />
         </DivTablero>
     );
 }
