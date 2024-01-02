181f87a62885cce97bf69eb313515b43dd7040c4
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-02 08:25:59 -0600

    se agrego el elemento divcenter a los titulos de las tareas

--- src/componentes/CardTarea.jsx
+++ src/componentes/CardTarea.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { DivCard } from "./Cajas";
+import { DivCard, DivCenter } from "./Cajas";
 
-export function CardTarea({tarea}){
-    return(
+export function CardTarea({ tarea }) {
+    return (
         <DivCard>
-            <h1  className="title-task">{tarea.Titulo}</h1>
+            <DivCenter>
+                <h1 className="title-task">{tarea.Titulo}</h1>
+            </DivCenter>
             <p className="content-task">{tarea.Descripcion}</p>
             <button>Completar</button>
         </DivCard>
