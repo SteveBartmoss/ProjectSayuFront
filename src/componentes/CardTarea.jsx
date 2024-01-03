f6f1d17e554ee3a8a6f32e0050c95d555c569c3f
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-02 20:56:31 -0600

    se agrego el center al boton

--- src/componentes/CardTarea.jsx
+++ src/componentes/CardTarea.jsx
@@ -8,7 +8,9 @@ export function CardTarea({ tarea }) {
                 <h1 className="title-task">{tarea.Titulo}</h1>
             </DivCenter>
             <p className="content-task">{tarea.Descripcion}</p>
-            <button>Completar</button>
+            <DivCenter>
+                <button>Completar</button>
+            </DivCenter>
         </DivCard>
     );
 }
