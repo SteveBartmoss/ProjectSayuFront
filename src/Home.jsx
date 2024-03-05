222fd8427643cfb88abfb9323077af9c20e68ad6
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-04 21:38:18 -0600

    se agregaron las listas de pruebas y completadas

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,12 +3,16 @@ import { DivTablero, GridListas } from "./componentes/Cajas";
 import { Pendientes } from "./componentes/Pendientes";
 import { Completadas } from "./componentes/Completadas";
 import { Footer } from "./componentes/Footer";
+import Progreso from "./componentes/Progreso";
+import Pruebas from "./componentes/Prueabas";
 
 export function Home() {
     return (
         <DivTablero>
             <GridListas>
                 <Pendientes />
+                <Progreso />
+                <Pruebas />
                 <Completadas />
             </GridListas>
             <Footer />
