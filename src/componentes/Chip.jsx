d9bac4c4970e7817f2837e80d81e7478930c917d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-14 12:08:17 -0600

    Se agrego la libreria

---
+++ src/componentes/Chip.jsx
@@ -0,0 +1,9 @@
+import './componentes.css';
+
+export function Chip({children, color="principal"}){
+    return(
+        <div className={`chip-${color}`}>
+            <span className="chip">{children}</span>
+        </div>
+    )
+}
