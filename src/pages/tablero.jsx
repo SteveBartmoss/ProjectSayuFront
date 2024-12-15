e043f65d04e2344a88a9a00638401e76335e6be0
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-14 18:03:05 -0600

    Se creo un componente para la vista principal del tablero

---
+++ src/pages/tablero.jsx
@@ -0,0 +1,14 @@
+
+
+export function Tablero(){
+    return(
+        <>
+            <div>
+                <h1>Barra superior</h1>
+            </div>
+            <div>
+                <h1>Tablero</h1>
+            </div>
+        </>
+    )
+}
