6d6f1e33482a54bc322f87fbc08b83d8ceaebd99
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 12:02:36 -0600

    Se creo un componente para la la lista de tareas

---
+++ src/pages/TaskList.jsx
@@ -0,0 +1,12 @@
+import { ExtendPanel } from "../componentes/extendpanel/ExtendPanel";
+
+
+export function TaskList({listTitle="",elements=[]}){
+    return(
+        <>
+            <ExtendPanel  title={listTitle}>
+                
+            </ExtendPanel>
+        </>
+    )
+}
