131d0ea945ced31b2dd96b57d1ca41f0854f5330
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 12:25:26 -0600

    Se implemento el listado de tareas que estan presentes en la lista de tareas

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -5,7 +5,15 @@ export function TaskList({listTitle="",elements=[]}){
     return(
         <>
             <ExtendPanel  title={listTitle}>
-                
+                {
+                    elements.map(task =>
+                        <div key={task.id}>
+                            <h1>{task.title}</h1>
+                            <p>{task.description}</p>
+                            <span>{task.state}</span>
+                        </div>
+                    )
+                }
             </ExtendPanel>
         </>
     )
