168660e98824212bde47babce1095c74f50a631d
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-12-30 11:29:30 -0600

    Se agrego la pagina para el login

--- src/Routes/routerMain.jsx
+++ src/Routes/routerMain.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RouterApp } from "./routerApp";
+import { Login } from "../layouts/login";
 
 export function RouterMain(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<h1>login</h1>} />
+                <Route path="/" element={<Login />} />
                 <Route path="/app/*" element={<RouterApp />} />
-                <Route path="login" element={<h1>Prueba</h1>} />
+                <Route path="login" element={<Login />} />
                 <Route path="badroute" element={<h1>505</h1>} />
                 <Route path="/documentation/*" element={<h1>Reto</h1>}/>
             </Routes>
