fa28f6e363269f9d010f7de5f2f55398db2b2a3c
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-08 21:37:42 -0600

    Se limpio el componente

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import { DivTablero, GridListas } from "./componentes/Cajas";
-import { Pendientes } from "./componentes/Pendientes";
-import { Completadas } from "./componentes/Completadas";
-import { Footer } from "./componentes/Footer";
-import Progreso from "./componentes/Progreso";
-import Pruebas from "./componentes/Prueabas";
 
 export function Home() {
     return (
-        <DivTablero>
-            <GridListas>
-                <Pendientes />
-                <Progreso />
-                <Pruebas />
-                <Completadas />
-            </GridListas>
-            <Footer />
-        </DivTablero>
+        <h1>Home</h1>
     );
 }
