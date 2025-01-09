1df6aa28a06e57e01053b8a0fb07e133efa7fe40
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-08 21:16:49 -0600

    Se actualizo la libreria

--- src/componentes/MenuBoton.jsx
+++ src/componentes/MenuBoton.jsx
@@ -1,14 +1,15 @@
 import { NavLink } from "react-router-dom";
-import { Icon } from "./Icon";
+import { Icon } from "./icon/Icon";
 
-export function MenuBoton({evento,isOpen,label=''}) {
+
+export function MenuBoton({evento,isOpen}) {
     return (
         <div className="div-menu-btn" onClick={evento}>
             <div className="element-menu-btn">
                 <Icon icono="menu" color="md-light" />
             </div>
             {
-                isOpen ? <div><NavLink className="menu-item" to='#'>{label}</NavLink></div> : <></>
+                isOpen ? <div><NavLink className="menu-item" to='#'>Ui Steve</NavLink></div> : <></>
             }
         </div>
     )
