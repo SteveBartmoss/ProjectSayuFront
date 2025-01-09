98fc425308cdfc9840a4c43b8ba0e9ad1bb2262d
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-08 21:36:51 -0600

    Se removio una importacion que no se usa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import './componentes/css/components.css'
 import { RouterTablero } from './Routes/Router';
 import { RouterMain } from './Routes/routerMain';
-import { Paper } from './componentes/Cajas';
 import { MainFrame } from './componentes/contenedores/Contenedores';
 
 function App() {
