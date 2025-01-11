4097eec73070a502d61d1c93cb20f8b755fb4688
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-11 10:40:23 -0600

    Se cambio la ruta para dirigir al router de la aplicacion

--- src/Routes/routerMain.jsx
+++ src/Routes/routerMain.jsx
@@ -7,7 +7,7 @@ export function RouterMain(){
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/app/*" element={<RouterApp />} />
+                <Route path="/*" element={<RouterApp />} />
                 <Route path="login" element={<Login />} />
                 <Route path="badroute" element={<h1>505</h1>} />
                 <Route path="/documentation/*" element={<h1>Reto</h1>}/>
