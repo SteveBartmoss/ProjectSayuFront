3abf7a26154026559c5819cca9551091f7133449
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-01-27 07:42:22 -0600

    Se agrego el modal para agregar los usuarios

--- src/pages/usuarios/Usuarios.jsx
+++ src/pages/usuarios/Usuarios.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { Btn } from "../../componentes/btn/Btn";
-import { Card } from "../../componentes/card/Card";
+import { Card, CardAccions } from "../../componentes/card/Card";
 import { DivCol, DivRow } from "../../componentes/contenedores";
 import { DataTable } from "../../componentes/datatable/DataTable";
 import { TextField } from "../../componentes/textField/TextField";
+import { Modal } from "../../componentes/modal/Modal";
 
 
 export function Usuarios() {
+
+    const [openUser, setOpenUser] = useState(false)
+
+
+
+    const openModalUser = () => {
+        setOpenUser(true)
+    }
+
+    const closeModalUser = () => {
+        setOpenUser(false)
+    }
 
     const encabezados = [
         {
@@ -46,11 +60,29 @@ export function Usuarios() {
                 <h1>Configuracion de usuarios</h1>
                 <DivRow>
                     <DivCol>
-                        <Btn>Agregar Usuario</Btn>
+                        <Btn evento={()=>openModalUser()}>Agregar Usuario</Btn>
                     </DivCol>
                 </DivRow>
                 <DataTable headers={encabezados} elements={elementos} />
             </Card>
+
+            <Modal estado={openUser} close={closeModalUser} persistent={true}>
+                <Card>
+                    <h1>Agregar Usuario</h1>
+                    <DivRow>
+                        <DivCol>
+                            <TextField label={'Nombre de Usuario'} />
+                        </DivCol>
+                        <DivCol>
+                            <TextField label={'Correo'} />
+                        </DivCol>
+                    </DivRow>
+                    <CardAccions>
+                        <Btn>Guardar</Btn>
+                        <Btn evento={()=>closeModalUser()} variant='' color='error'>Cancelar</Btn>
+                    </CardAccions>
+                </Card>
+            </Modal>
         </>
     )
 }
