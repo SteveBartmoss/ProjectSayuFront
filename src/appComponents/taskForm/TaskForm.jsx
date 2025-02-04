9f31977a4a500e5abeac5395630c8fc64a8c2dd3
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-02-04 08:05:58 -0600

    Se agrego la implementacion para agregar una tarea

--- src/appComponents/taskForm/TaskForm.jsx
+++ src/appComponents/taskForm/TaskForm.jsx
@@ -1,27 +1,52 @@
+import { useState } from "react";
 import { Btn } from "../../componentes/btn/Btn";
 import { Card, CardAccions } from "../../componentes/card/Card";
 import { DivCol, DivRow } from "../../componentes/contenedores";
 import { TextField } from "../../componentes/textField/TextField";
-
+import { useDispatch } from "react-redux";
+import {addTask} from "../../features/taskSlice"
 
 export function TaskForm(){
+
+    const dispach = useDispatch()
+
+    const [title,setTitle]=useState('')
+    const [description,setDescription]=useState('')
+
+    const handleTitle=(event)=>{
+        setTitle(event.target.value)
+    }
+
+    const handleDescription=(event)=>{
+        setDescription(event.target.value)
+    }
+
+    const createTask=()=>{
+        dispach(addTask({
+            id: 1,
+            title: title,
+            description: description,
+            state: "creada",
+        }))
+    }
+
     return(
         <Card>
             <h1>Crear una nueva tarea</h1>
 
             <DivRow>
                 <DivCol>
-                    <TextField label={'Titulo'} />
+                    <TextField label={'Titulo'} text={title} action={handleTitle} />
                 </DivCol>
             </DivRow>
             <DivRow>
                 <DivCol>
-                    <TextField label={'Descripcion'} />
+                    <TextField label={'Descripcion'} text={description} action={handleDescription} />
                 </DivCol>
             </DivRow>
 
             <CardAccions>
-                <Btn>Crear tarea</Btn>
+                <Btn evento={()=>createTask()}>Crear tarea</Btn>
             </CardAccions>
         </Card>
     )
