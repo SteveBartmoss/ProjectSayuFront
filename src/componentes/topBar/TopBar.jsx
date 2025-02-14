c3b54649ae3534e049a6a66eceec48cb9e3bdb59
kevinho1908 <kevingonzalez01@hotmail.com>
2025-02-14 16:10:18 -0600

    modifique el topbar

--- src/componentes/topBar/TopBar.jsx
+++ src/componentes/topBar/TopBar.jsx
@@ -1,13 +1,31 @@
 import './TopBar.css';
+import { FaHome, FaTasks, FaUser, FaCog } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-export function TopBar(){
-    return(
-        <>
-            <div className="top-bar-background">
-                <span>User image</span>
-                <span>Config menu</span>
-                <span>Top Bar</span>
-            </div>
-        </>
-    )
+export function TopBar() {
+    return (
+        <div className="top-bar-background">
+            <nav className="top-bar-nav">
+                <Link to="/" className="nav-item">
+                    <FaHome className="nav-icon" />
+                    <span>Inicio</span>
+                </Link>
+                
+                <Link to="/tareas" className="nav-item">
+                    <FaTasks className="nav-icon" />
+                    <span>Tareas</span>
+                </Link>
+                
+                <Link to="/perfil" className="nav-item">
+                    <FaUser className="nav-icon" />
+                    <span>Perfil</span>
+                </Link>
+                
+                <Link to="/configuracion" className="nav-item">
+                    <FaCog className="nav-icon" />
+                    <span>Configuración</span>
+                </Link>
+            </nav>
+        </div>
+    );
 }
